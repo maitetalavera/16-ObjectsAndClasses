1d08b16dae27e3692b3bef61d7681bb009ee2049
maitetalavera <mtalavera001@student.franciscan.edu>
2024-03-19 11:42:06 -0400

    completed m3

--- src/m3_create_objects.py
+++ src/m3_create_objects.py
@@ -1,5 +1,20 @@
+class Pet:
+    def __init__(self, name, age):
+        self.name=name
+        self.age=age
+    
+    def __str__(self):
+        return f"Name: {self.name}, Age: {self.age}"
+    
+    def speak(self):
+        print(f"{self.name}: Bark! Bark! Bark!")
+    
+p1=Pet("Fido", 4)
+print(p1)
+p1.speak()
+
 ###############################################################################
-# TODO: 1. (1 pt)
+# DONE: 1. (1 pt)
 #
 #   In this module, you will need the Pet class that you created in m2. Copy
 #   and paste the class above this _todo_ for you to use in the rest of these
@@ -11,8 +26,10 @@
 #   Once you have done this, then change the above _TODO_ to DONE.
 ###############################################################################
 
+p2=Pet("Bob", 5)
+
 ###############################################################################
-# TODO: 2. (1 pt)
+# DONE: 2. (1 pt)
 #
 #   For this _todo_, print your object using it's variable name. Notice how it
 #   prints according to your __str__() method definition.
@@ -20,8 +37,10 @@
 #   Once you have done this, then change the above _TODO_ to DONE.
 ###############################################################################
 
+print(p2)
+
 ###############################################################################
-# TODO: 3. (1 pt)
+# DONE: 3. (1 pt)
 #
 #   For this _todo_, using the object you have defined in this todo, call the
 #   speak() method on that object.
@@ -29,8 +48,10 @@
 #   Once you have done this, then change the above _TODO_ to DONE.
 ###############################################################################
 
+p2.speak()
+
 ###############################################################################
-# TODO: 4. (1 pt)
+# DONE: 4. (1 pt)
 #
 #   For this _todo_, modify the age property of your object to be something
 #   different than it was originally. Make sure you print your object again to
@@ -38,3 +59,6 @@
 #
 #   Once you have done this, then change the above _TODO_ to DONE.
 ###############################################################################
+
+p2.age= 3
+print(p2)
